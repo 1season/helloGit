64571b1b35baedd04ca5a34407b6c81c26516e68
wi <sxm15589075579551997@outlook.com>
2023-07-09 18:40:21 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/wi/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.wi.git;
+
+/**
+ * @author:wi
+ * @create: 2023-07-09 18:30
+ * @Description:
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
